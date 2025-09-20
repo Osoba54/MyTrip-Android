01d3bcac3452514c6fc589215bb237523d829158
Osoba54 <ksawery112007@gmail.com>
2025-09-20 12:51:07 +0200

    feat: add AuthRepositoryImpl

--- app/src/main/java/com/example/mytrip/repositories/AuthRepository.kt
+++ app/src/main/java/com/example/mytrip/repositories/AuthRepository.kt
@@ -1,35 +1,9 @@
 package com.example.mytrip.repositories
 
-import com.example.mytrip.authApi.AuthApiService
 import com.example.mytrip.authApi.LoginRequest
-import com.example.mytrip.authApi.TokenManager
-import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
-class AuthRepository @Inject constructor(
-    private val authApiService: AuthApiService,
-    private val tokenManager: TokenManager
-) {
-
-    suspend fun login(request: LoginRequest){
-        val response = authApiService.login(request)
-        if(response.isSuccessful){
-            val tokenResponse = response.body()
-            if (tokenResponse != null) {
-                tokenManager.saveTokens(tokenResponse.access, tokenResponse.refresh)
-            }
-        } else {
-            throw Exception("Nie udane logowanie: ${response.code()}")
-        }
-    }
-
-    fun getAccessToken(): String?{
-        return tokenManager.getAccessToken()
-    }
-
-    fun logout(){
-        tokenManager.clearTokens()
-    }
-
+interface AuthRepository {
+    suspend fun login(request: LoginRequest)
+    fun getAccessToken(): String?
+    fun logout()
 }
---
+++ app/src/main/java/com/example/mytrip/repositories/AuthRepositoryImpl.kt
@@ -0,0 +1,47 @@
+package com.example.mytrip.repositories
+
+import android.content.Context
+import com.example.mytrip.R
+import com.example.mytrip.authApi.AuthApiService
+import com.example.mytrip.authApi.LoginRequest
+import com.example.mytrip.authApi.TokenManager
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AuthRepositoryImpl @Inject constructor(
+    private val authApiService: AuthApiService,
+    private val tokenManager: TokenManager,
+    private val context: Context
+) : AuthRepository {
+
+    override suspend fun login(request: LoginRequest){
+        val response = authApiService.login(request)
+        if(response.isSuccessful){
+            val tokenResponse = response.body()
+            if (tokenResponse != null) {
+                tokenManager.saveTokens(tokenResponse.access, tokenResponse.refresh)
+            }
+        } else {
+            val msg: String = when(response.code()) {
+                400 -> context.getString(R.string.error_400)
+                401 -> context.getString(R.string.error_401)
+                403 -> context.getString(R.string.error_403)
+                404 -> context.getString(R.string.error_404)
+                500 -> context.getString(R.string.error_500)
+                503 -> context.getString(R.string.error_503)
+                else -> context.getString(R.string.error_unknown, response.code())
+            }
+            throw Exception("${context.getString(R.string.text_login_failed)}: $msg")
+        }
+    }
+
+    override fun getAccessToken(): String?{
+        return tokenManager.getAccessToken()
+    }
+
+    override fun logout(){
+        tokenManager.clearTokens()
+    }
+
+}
