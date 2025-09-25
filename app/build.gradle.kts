ad5a02730da9a4a38c71a50fdc598c947c0be2e0
Osoba54 <ksawery112007@gmail.com>
2025-09-25 20:02:47 +0200

    ref: Move dependencies to libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,21 +61,21 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-//    TODO Dodać do .toml
-    implementation("androidx.navigation:navigation-compose:2.8.0")
-    implementation("androidx.compose.material:material-icons-extended:1.7.2")
-    // Retrofit i GSON converter
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    // OkHttp (dla interceptorów)
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
-    // Jetpack Security
-    implementation("androidx.security:security-crypto:1.1.0-alpha06")
-//    implementation("com.google.dagger:hilt-android:2.50")
-//    hilt i ksp
+
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.compose.material.icons.extended)
+
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+
+    implementation(libs.androidx.security.crypto)
+
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
 }
